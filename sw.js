49b56f3cba4c8e573314eb47b587879ec17517a5
kipetchis <kip3tchis@gmail.com>
2026-06-12 12:45:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'oracle-v84'; // ⚠️ Incrémenter à CHAQUE déploiement
+const VERSION = 'oracle-v85'; // ⚠️ Incrémenter à CHAQUE déploiement
 
 // Fichiers critiques à pré-cacher à l'installation (légers, indispensables au démarrage)
 // Les mp3 ne sont PLUS pré-cachés : ils sont mis en cache au vol à la première lecture
